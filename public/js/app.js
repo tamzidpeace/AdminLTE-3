b5f666a9719f7a27b4f5b1d2be8fba65be626d7e
Arafat Kamal <tamjedpeace@gmail.com>
2020-09-22 07:03:15 +0600

    remove the public from url

--- public/js/app.js
+++ public/js/app.js
@@ -49878,8 +49878,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Laravel\LPV2\AdminLTE-3\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Laravel\LPV2\AdminLTE-3\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\sms\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\sms\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
